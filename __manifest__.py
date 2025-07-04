bce98a5968403aa78d8f996d9db0d000b9505070
Nemael <panouillot.mael@gmail.com>
2025-07-04 00:57:07 -0500

    First layout of the addon

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Taler-Odoo',
+    'version': '1.0',
+    'author': 'Nemael',
+    'category': 'Custom',
+    'summary': 'Taler payment system for Odoo',
+    'depends': ['base'],
+    'data': ['views/my_model_view.xml'],
+    'installable': True,
+}
