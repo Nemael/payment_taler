a3a136a1bcd87a6eae1d0ae1a8f2ecfe0d6d9939
Nemael <panouillot.mael@gmail.com>
2025-07-17 00:04:21 -0500

    applied fixes to order placing, you can now pay something through taler using a button on Odoo's interface.

--- models/models.py
+++ models/models.py
@@ -15,7 +15,7 @@ class Student(models.Model):
     name3 = fields.Char(string="Name3")
     name4 = fields.Char(string="Name4")
     taler_url = fields.Char(string="Taler URL", default="https://backend.demo.taler.net/instances/sandbox")
-    token = fields.Char(string="Current Token")
+    secret_token = fields.Char(string="Current Secret Token")
     test_order_id = fields.Char(string="Test OrderID", default="2025.191-02D3GX2J9ME3M")
 
     order_summary  = fields.Char(string="Order Summary", default="Test order")
@@ -23,6 +23,9 @@ class Student(models.Model):
     order_currency = fields.Char(string="Order Currency", default="KUDOS")
     order_amount = fields.Char(string="Order Amount", default="1")
     order_latest_order_id = fields.Char(string="Latest Order Id", default="1")
+    order_last_order_url = fields.Char(string="Last Order URL", default="")
+
+    last_order_uri = fields.Char(string="Last Order URI", default="")
 
     def changeRecord(self):
         self.name4 = self.name4 + '+'
@@ -37,16 +40,18 @@ class Student(models.Model):
             "User-Agent": "TalerOdoo",
             "Authorization": "Bearer secret-token:sandbox"
         }
+        talog("Headers: ", headers)
+        talog("Payload: ", payload)
         response = requests.request("POST", url, json=payload, headers=headers)
-        print("Request received")
+        print("Response received")
         talog(response.text)
         if response.status_code != 200:
             talog("Error getting token, bad response: ", response.text)
             return
-        if response.json()["token"] is None:
+        if "token" not in response.json():
             talog("Error getting new token: ", response.text)
             return
-        self.token = response.json()["token"]
+        self.secret_token = response.json()["token"]
 
     def requestGetOrderFromId(self):
         url = self.taler_url + "/private/orders/" + self.test_order_id
@@ -55,15 +60,27 @@ class Student(models.Model):
             "User-Agent": "TalerOdoo",
             "Authorization": "Bearer " + self.token
         }
+        talog("Headers: ", headers)
+        talog("Payload: ", payload)
         response = requests.request("GET", url, data=payload, headers=headers)
+        print("Response received")
         talog(response.text)
+        if response.status_code != 200:
+            talog("Error getting order from id, bad response: ", response.text)
+            return
 
     def requestGetConfig(self):
         url = self.taler_url + "/config"
         payload = ""
         headers = {"User-Agent": "TalerOdoo"}
+        talog("Headers: ", headers)
+        talog("Payload: ", payload)
         response = requests.request("GET", url, data=payload, headers=headers)
+        print("Response received")
         talog(response.text)
+        if response.status_code != 200:
+            talog("Error getting config, bad response: ", response.text)
+            return
 
     def postPlaceOrder(self):
         url = self.taler_url + "/private/orders"
@@ -78,14 +95,38 @@ class Student(models.Model):
         headers = {
             "Content-Type": "application/json",
             "User-Agent": "TalerOdoo/insomnia/11.3.0",
-            "Authorization": "Bearer secret-token:BSE1SCQR7B8WKR9PW79K6VV9K99RND95HNX0VPW9B4JVR8DYQAQG"
+            "Authorization": "Bearer " + self.secret_token
         }
+        talog("Headers: ", headers)
+        talog("Payload: ", payload)
         response = requests.request("POST", url, json=payload, headers=headers)
+        print("Response received")
         talog(response.text)
         if response.status_code != 200:
-            talog("Error getting token, bad response: ", response.text)
+            talog("Error placing order token, bad response: ", response.text)
             return
-        if response.json()["order_id"] is None:
+        if "order_id" not in response.json():
             talog("Error getting new order_id: ", response.text)
             return
         self.order_latest_order_id = response.json()["order_id"]
+        self.order_last_order_url = self.taler_url + "/orders/" + self.order_latest_order_id
+
+    def getOrderTalerUri(self):
+        url = "https://backend.demo.taler.net/instances/sandbox/private/orders/" + self.order_latest_order_id
+
+        payload = ""
+        headers = {
+            "User-Agent": "TalerOdoo/insomnia/11.3.0",
+            "Authorization": "Bearer " + self.secret_token
+        }
+        talog("Headers: ", headers)
+        talog("Payload: ", payload)
+        response = requests.request("GET", url, data=payload, headers=headers)
+        print("Response received")
+        if response.status_code != 200:
+            talog("Error getting order talery payment URI, bad response: ", response.text)
+            return
+        if "taler_pay_uri" not in response.json():
+            talog("Error getting taler_pay_uri field: ", response.text)
+            return
+        self.last_order_uri = response.json()["taler_pay_uri"]
