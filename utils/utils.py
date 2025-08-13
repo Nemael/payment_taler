3c4d595434bac6d73075c8a49781c3552050d5cc
gabriel.ku <gabriel.ku@fsfe.org>
2025-08-13 14:49:31 +0200

    Updating select files with Copyright and Licensing Information (REUSE)

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Mael Panouillot <panouillot.mael@gmail.com>
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
 class Color:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
