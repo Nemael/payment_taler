0ba0d89b17cd1544ff30eec68d8eaa05630a80a9
Nemael <panouillot.mael@gmail.com>
2025-12-21 00:43:57 -0600

    Added a comment

--- models/taler_provider.py
+++ models/taler_provider.py
@@ -56,15 +56,15 @@ class TalerProvider(models.Model):
         return const.DEFAULT_PAYMENT_METHOD_CODES
 
     def merchant_url_check_button(self):
-        print("==============================================================================================================")
+        # Checks the validity of the URL set by the user
         response = getMerchantConfiguration(self.taler_merchant_url)
         if not response["name"] or not response["name"] == "taler-merchant" or not response["version"] or not response["currencies"]:
             raise ValidationError("The Taler Merchant URL is invalid")
 
+        # Checks if the currencies used by Odoo are all included in the supported currencies by the Taler Merchant
         merchant_currencies = []
         for currency in response["currencies"].keys():
             merchant_currencies.append(currency)
-
         odoo_currencies_missing_in_merchant = []
         for odoo_currency in const.SUPPORTED_CURRENCIES:
             if odoo_currency not in merchant_currencies:
@@ -72,8 +72,7 @@ class TalerProvider(models.Model):
         if len(odoo_currencies_missing_in_merchant) > 0:
             raise ValidationError("The Taler Merchant URL is invalid.\nCurrencies supported on Odoo side: " + str(odoo_currencies_missing_in_merchant) + ".\nCurrencies supported on Taler merchant side: " + str(merchant_currencies) + ".\nDiscrepancy: " + str(odoo_currencies_missing_in_merchant))
 
-
-
+        # If everything is ok, create an "ok" popup for the user
         confirmation_string_for_user = "The Taler Merchant URL is valid and the currencies are compatible. "
         confirmation_string_for_user += "Merchant name: " + response["name"]
         confirmation_string_for_user += ". Merchant version: " + response["version"]
