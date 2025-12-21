99ef05807c58df88b6dd9fb028eb2ac9a7fea0c0
Nemael <panouillot.mael@gmail.com>
2025-12-20 20:22:37 -0600

    Cleaned utils

--- utils/utils.py
+++ utils/utils.py
@@ -3,9 +3,6 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 import uuid
 
-class Color:
-    PURPLE = '\033[95m'
-
 from datetime import datetime, time, timedelta
 import os
 import logging
@@ -13,23 +10,7 @@ import qrcode
 import base64
 from io import BytesIO
 
-# _logger = logging.getLogger("TOPS")
 _logger = logging.getLogger(__name__)
-
-# class Color:
-#     PURPLE = '\033[95m'
-#     CYAN = '\033[96m'
-#     DARKCYAN = '\033[36m'
-#     BLUE = '\033[94m'
-#     GREEN = '\033[92m'
-#     YELLOW = '\033[93m'
-#     RED = '\033[91m'
-#     BOLD = '\033[1m'
-#     UNDERLINE = '\033[4m'
-#     END = '\033[0m'
-#
-#     def __init__(self):
-#        pass
 
 def talog(*args):
     log = ""
@@ -56,25 +37,22 @@ def taerror(*args):
     _logger.error(log)
 
 def generate_qr(url):
-    print("????? Generating QR code")
     qr = qrcode.QRCode(box_size=10, border=2)
     qr.add_data(url)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-
     buf = BytesIO()
     img.save(buf, format='PNG')
     qr_bytes = buf.getvalue()
-    print("{{{{{{{{{{{{{{", base64.b64encode(qr_bytes).decode("utf-8"))
 
     return base64.b64encode(qr_bytes).decode("utf-8")
 
 def generate_UUID():
-    return(uuid.uuid4())
+    return uuid.uuid4()
 
 def get_datetime_date_to_epoch(my_date):
-    #time.min represents the minimum time in the day, and sets the epoch time to the beginning of the specified day
+    # time.min represents the minimum time in the day, and the following line sets the epoch time to the beginning of the specified day
     return int(datetime.combine(my_date, time.min).timestamp())
 
-def get_datetime_now_to_epoch(add_minutes):
-    return int((datetime.now() + timedelta(minutes=add_minutes)).timestamp())
+def get_datetime_now_to_epoch(minutes_to_add):
+    return int((datetime.now() + timedelta(minutes=minutes_to_add)).timestamp())
