4a91200dba4192321d4b874d76e43ff950cd2e24
Nemael <panouillot.mael@gmail.com>
2026-02-01 10:41:12 -0600

    Fix to check currencies button

--- models/taler_provider.py
+++ models/taler_provider.py
@@ -62,7 +62,7 @@ class TalerProvider(models.Model):
         return const.DEFAULT_PAYMENT_METHOD_CODES
 
     def merchant_url_check_button(self):
-        """ Checks the validity of the URL set by the user. """
+        """ Checks the validity of the Taler merchant URL set by the user. """
         response = getMerchantConfiguration(self.taler_merchant_url)
         if not response["name"] or not response["name"] == "taler-merchant" or not response["version"] or not response["currencies"]:
             raise ValidationError("The Taler Merchant URL is invalid")
@@ -71,10 +71,11 @@ class TalerProvider(models.Model):
         merchant_currencies = []
         for currency in response["currencies"].keys():
             merchant_currencies.append(currency)
+
         odoo_currencies_missing_in_merchant = []
-        for odoo_currency in const.SUPPORTED_CURRENCIES:
-            if odoo_currency not in merchant_currencies:
-                odoo_currencies_missing_in_merchant.append(odoo_currency)
+        for odoo_currency in self.available_currency_ids:
+            if odoo_currency.name not in merchant_currencies:
+                odoo_currencies_missing_in_merchant.append(odoo_currency.name)
         if len(odoo_currencies_missing_in_merchant) > 0:
             raise ValidationError("The Taler Merchant URL is invalid.\nCurrencies supported on Odoo side: " + str(odoo_currencies_missing_in_merchant) + ".\nCurrencies supported on Taler merchant side: " + str(merchant_currencies) + ".\nDiscrepancy: " + str(odoo_currencies_missing_in_merchant))
 
